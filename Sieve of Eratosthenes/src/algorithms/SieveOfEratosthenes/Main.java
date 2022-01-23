d532d728a591b1a5ae682eb8752308e76435205a
Thomas M <boomlich@gmail.com>
2022-01-23 03:46:03 +0100

    Added major optimalizations

--- Sieve of Eratosthenes/src/algorithms/SieveOfEratosthenes/Main.java
+++ Sieve of Eratosthenes/src/algorithms/SieveOfEratosthenes/Main.java
@@ -1,34 +1,56 @@
 package algorithms.SieveOfEratosthenes;
 
-import java.util.ArrayList;
-
 public class Main {
-    public static int N = 100;
-    public static ArrayList<Integer> primeNumbers = new ArrayList<>();
-
     public static void main(String[] args) {
+        int N = 100;
+        System.out.println(findAllPrimes(N));
+    }
 
-        // Add all numbers to a list
+    public static String findAllPrimes(int N) {
+        String primes = "";
+        int[] allNumbers = new int[N-1];
+        int[] P = {0, 0};
+
+        // Make array with all number from 2 to N
+        int j = 0;
         for (int i = 2; i < N+1; i++) {
-            primeNumbers.add(i);
+            allNumbers[j] = i;
+            j++;
         }
 
-        int i = 0;
-        while (primeNumbers.get(i) < Math.sqrt(N)) {
-            int P = primeNumbers.get(i);
-            int multipleP = P;
+        while (P[1] * P[1] < N){
+            P = findPrime(allNumbers);
+            allNumbers = crossNum(allNumbers, P);
+            primes += P[1] + " ";
+        }
 
-            int j = 2;
-            while (multipleP < N-1) {
-                multipleP = P * j;
-                primeNumbers.remove(Integer.valueOf(multipleP));
-                j ++;
+        for (int x: allNumbers) {
+            if (x != 0){
+                primes += x + " ";
             }
+        }
+        return primes;
+    }
+
+    // Set the prime and all it's multiples to 0
+    static int[] crossNum(int[] inputArray, int[] prime) {
+        int arrayLen = inputArray.length;
+        for (int i = prime[0]; i < arrayLen; i+= prime[1]) {
+            if (inputArray[i] != 0) {
+                inputArray[i] = 0;
+            }
+        }
+        return inputArray;
+    }
+
+    // Find the prime and it's index in the array
+    static int[] findPrime(int[] inputArray) {
+        int P = 0;
+        int i = 0;
+        while (P == 0) {
+            P = inputArray[i];
             i ++;
         }
-
-        System.out.println("All prime numbers up to " + N +":");
-        System.out.println(primeNumbers);
+        return new int[]{i-1, P};
     }
 }
-
